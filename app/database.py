06809b9672ee500959e9a4ea314b68fd3a53b5d3
panda-with-issues <ryoda94@gmail.com>
2025-07-04 10:43:32 +0200

    corretto gli errori del database, popolato il database con dati di prova

--- app/database.py
+++ app/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import CheckConstraint, ForeignKeyConstraint, DDL, event, ForeignKey
 from typing import Literal, Optional
 import datetime
+import sqlite3
 
 def init_db(app):
   class Base(DeclarativeBase):
@@ -21,10 +22,11 @@ def init_db(app):
 
     user_name: Mapped[str] = mapped_column(primary_key=True)
     password: Mapped[str]
+    is_admin: Mapped[bool]
 
   # Mercati
 
-  Giorno = Literal['Lunedì', 'Mertedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica']
+  Giorno = Literal['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica']
 
   class Mercati(db.Model):
     __tablename__ = 'Mercati'
@@ -35,33 +37,19 @@ def init_db(app):
     is_attuale: Mapped[Optional[bool]]
 
     __table_args__ = (
+       CheckConstraint(
+        "giorno IN ('Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica')",
+        name='giorno_dominio'
+      ),
       CheckConstraint(
         '(is_evento = 1 AND is_attuale IS NULL) OR (is_evento = 0 AND is_attuale IS NOT NULL)',
         name="check_is_attuale"
       ),
       CheckConstraint(
-        "is_attuale = 0 AND nome LIKE '%_old'",
-        name="check_nome_old"
+        "(is_attuale = 0 AND nome LIKE '%_old') OR (is_attuale IS NULL OR is_attuale != 0)",
+        name="check_nome_old",
       )
     )
-
-  old_trigger = DDL("""
-    CREATE TRIGGER IF NOT EXISTS trg_append_old
-      AFTER UPDATE ON mercati
-      FOR EACH ROW
-      WHEN 
-        NEW.is_attuale = 0
-        AND OLD.is_attuale IS NOT 0
-        AND substr(NEW.nome, -4) != '_old'
-      BEGIN
-        UPDATE mercati
-        SET nome = nome || '_old'
-        WHERE nome = NEW.nome AND giorno = NEW.giorno;
-      END;
-      """
-  )
-
-  event.listen(Mercati.__table__, 'after_create', old_trigger)
 
   # Corrispettivi
 
@@ -113,7 +101,45 @@ def init_db(app):
   db.init_app(app)
 
   with app.app_context():
-    print("say")
+    # attiviamo il vincolo di chiave esterna che in SQLite non è attivo di default
+    @event.listens_for(db.engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+      if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
+
+    with db.engine.connect() as conn:
+      res = conn.execute(db.text("PRAGMA foreign_keys")).scalar()
+      print("FOREIGN KEYS ATTIVI:", res)  # Deve stampare 1
+    
     db.create_all()
 
+    # popoliamo il database con dati di prova
+    db.session.add_all([
+      Utenti(
+        user_name='Dario',
+        password='pw',
+        is_admin=True
+      ),
+      Utenti(
+        user_name='Yuuki',
+        password='pw',
+        is_admin=False
+      ),
+      Mercati(
+        nome='Centro',
+        giorno='Sabato',
+        is_evento=False,
+        is_attuale=True
+      ),
+      Mercati(
+        nome='Centro_old',
+        giorno='Martedì',
+        is_evento=False,
+        is_attuale=False
+      )
+    ])
+    db.session.commit()
+    
   return db
